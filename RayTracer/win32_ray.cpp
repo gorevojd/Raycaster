d98f9ef8e68a8348bcbac1772265c1d141da5cdb
Dima <gorevojd27@gmail.com>
2017-10-29 11:51:43 +0300

    Minor commit

--- RayTracer/win32_ray.cpp
+++
@@ -1,8 +0,0 @@
-#include "win32_ray.h"
-
-#include <Windows.h>
-
-#ifndef INTERNAL_FUNCTION
-#define INTERNAL_FUNCTION static
-#endif
-
--- RayTracer/win32_ray.h
+++
@@ -1,8 +0,0 @@
-#ifndef WIN32_RAY_H
-#define WIN32_RAY_H
-
-#include "ray.h"
-
-extern b32 RenderTile(work_queue* Queue);
-
-#endif
